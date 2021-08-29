6517bdb6519fd0790efd23ca03dd7483d233134e
yohei-ny <60955704+yohei-ny@users.noreply.github.com>
2021-08-29 19:10:31 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   get 'todolists/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'top' => 'homes#top'
-  # post 'todolists' => 'todolists#create'
   get 'todolists' => 'todolists#index'
   # .../todolists/1 や .../todolists/3 に該当する
   get 'todolists/:id' => 'todolists#show', as: 'todolist'
